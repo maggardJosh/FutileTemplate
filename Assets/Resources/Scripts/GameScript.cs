e59bb2b8c9fe7559c3fb6fdc4244871f9cc4c9dd
Josh Maggard <jmaggard62@gmail.com>
2015-01-10 17:10:32 -0600

    Basic texturepacker stuff

--- Assets/Resources/Scripts/GameScript.cs
+++ Assets/Resources/Scripts/GameScript.cs
@@ -6,13 +6,17 @@ public class GameScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         FutileParams futileParams = new FutileParams(true, false, false, false);
-        futileParams.AddResolutionLevel(160.0f, 1.0f, 1.0f, ""); // Gameboy resolution 160x144px
+        futileParams.AddResolutionLevel(480.0f, 1.0f, 1.0f, "");
 
         futileParams.origin = new Vector2(0.5f, 0.5f);
         futileParams.backgroundColor = new Color(0, 0, 0);
         futileParams.shouldLerpToNearestResolutionLevel = true;
 
         Futile.instance.Init(futileParams);
+
+        Futile.atlasManager.LoadAtlas("Atlases/inGameAtlas");
+
+        Futile.stage.AddChild(new FSprite("white"));
 
 	}
 	
